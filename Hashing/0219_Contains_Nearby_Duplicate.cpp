86dedcfc17b0f948e94d27e77d76878ce089b448
Shivansh Tiwari <tiwarishivansh022@gmail.com>
2026-08-21 18:08:01 +0530

    Add solution for LeetCode #219 - Contains Duplicate II
    Implemented a solution for LeetCode problem #219 using a hash map to track indices.

---
+++ Hashing/0219_Contains_Nearby_Duplicate.cpp
@@ -0,0 +1,20 @@
+// LeetCode #219 - Contains Duplicate II
+// Approach: Hash Map storing latest index
+// Average Time: O(n)
+// Space: O(n)
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,int>mp;
+        for(int i = 0; i < nums.size(); i++){
+            if(mp.count(nums[i])){
+                if(i-mp[nums[i]]<=k){
+                    return true;
+                }
+            }
+            mp[nums[i]]= i;
+        }
+        return false;
+    }
+};
